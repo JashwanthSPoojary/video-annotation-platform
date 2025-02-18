ed3685e7ba420016d46460703b1a06b5468cfbeb
anonomous <125535128+Jashwanth1244@users.noreply.github.com>
2025-02-18 19:09:49 +0530

    auth option error 3

--- app/api/videos/route.ts
+++ app/api/videos/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 import { createVideoSchema } from "@/schemas/annotation";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../auth/[...nextauth]/route";
+import { authOptions } from "@/lib/auth";
 
 export async function POST(request: Request) {
   try {
--- components/features/dashboard/AnnotatedVideosList.tsx
+++ components/features/dashboard/AnnotatedVideosList.tsx
@@ -2,9 +2,9 @@ import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
 import { Button } from "@/components/ui/button"
 import { prisma } from "@/lib/prisma";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
 import AnnotationsButton from "./AnnotationsButton";
+import { authOptions } from "@/lib/auth";
 
 export default async function AnnotatedVideosList() {
 
