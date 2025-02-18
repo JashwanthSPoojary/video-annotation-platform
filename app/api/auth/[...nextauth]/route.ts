3bebbf7dcfb4434a1ab4c3410d6b36b606785525
anonomous <125535128+Jashwanth1244@users.noreply.github.com>
2025-02-18 18:40:47 +0530

    auth option error

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,39 +1,6 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
-import GithubProvider from "next-auth/providers/github";
-import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { prisma } from "@/lib/prisma";
+import NextAuth from "next-auth";
 
-export const authOptions: NextAuthOptions = {
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID as string,
-      clientSecret: process.env.GITHUB_SECRET_ID as string,
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_SECRET_ID as string,
-    }),
-  ],
-  adapter: PrismaAdapter(prisma),
-  secret:process.env.NEXTAUTH_SECRET as string,
-  callbacks: {
-    session: async ({ session, token }) => {
-      if (session.user) {
-        session.user.id = token.sub as string;
-      }
-      return session;
-    },
-  },
-  session: {
-    strategy: "jwt", 
-  },
-  pages:{
-    signIn:"/signin",
-    error:"/signin/error",
-    
-  }
-};
+import { authOptions } from "@/lib/auth";
 
 const handler = NextAuth(authOptions);
 
---
+++ lib/auth.ts
@@ -0,0 +1,34 @@
+const authOptions: NextAuthOptions = {
+    providers: [
+      GithubProvider({
+        clientId: process.env.GITHUB_CLIENT_ID as string,
+        clientSecret: process.env.GITHUB_SECRET_ID as string,
+      }),
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID as string,
+        clientSecret: process.env.GOOGLE_SECRET_ID as string,
+      }),
+    ],
+    adapter: PrismaAdapter(prisma),
+    secret:process.env.NEXTAUTH_SECRET as string,
+    callbacks: {
+      session: async ({ session, token }) => {
+        if (session.user) {
+          session.user.id = token.sub as string;
+        }
+        return session;
+      },
+    },
+    session: {
+      strategy: "jwt", 
+    },
+    pages:{
+      signIn:"/signin",
+      error:"/signin/error",
+      
+    }
+  };
+
+  export {
+    authOptions
+  }
